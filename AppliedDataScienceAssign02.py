fe3bf50823f0d3f56c1412b0e534930b6f5adb73
Muhammad Ali <127026879+SESWAMODELSCHOOL@users.noreply.github.com>
2023-04-06 00:00:15 +0100

    Update AppliedDataScienceAssign02.py

--- AppliedDataScienceAssign02.py
+++ AppliedDataScienceAssign02.py
@@ -4,6 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+## Getting data from files
+
 pd.set_option('display.max_columns', None)
 
 df = pd.read_csv('API_19_DS2_en_csv_v2_4756035.csv',skiprows=4)
@@ -14,13 +16,19 @@ dfc1 = dfc[~pd.isna(dfc['IncomeGroup'])]
 df = df[['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code','2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012',
          '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']]
 
+
 df['mean'] = df[[ '2005', '2006', '2007','2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
        '2017', '2018', '2019', '2020', '2021']].mean(axis=1)
+
 selected_indicators = ['Urban population (% of total population)', 'Urban population growth (annual %)', 'Population, total', 'Population growth (annual %)',
- 'Agricultural land (sq. km)', 'Energy use (kg of oil equivalent per capita)','Electric power consumption (kWh per capita)','Access to electricity (% of population)','Forest area (% of land area)','Arable land (% of land area)', 'Agricultural land (% of land area)']
+ 'Agricultural land (sq. km)', 'Energy use (kg of oil equivalent per capita)','Electric power consumption (kWh per capita)','Access to electricity (% of population)',
+ 'Forest area (% of land area)','Arable land (% of land area)', 'Agricultural land (% of land area)']
+
+
 
 countries = ['Italy','Australia','China','United Arab Emirates','Germany','France',
              'Malaysia','Japan','New Zealand','Morocco','United States','Pakistan','India','Bangladesh','Thailand']
+
 
 df = df.drop(['Country Code','Indicator Code'], axis=1)
 
@@ -32,7 +40,6 @@ def data_ingestion(df, indicator):
     df2 = df1.transpose()
     return df1, df2
 
-#df_year, df_country = data_ingestion(df, 'Population, total')
 
 
 for ind in selected_indicators:
@@ -44,6 +51,8 @@ for ind in selected_indicators:
     plt.title(ind)
     plt.xlabel('2005-2021')
     plt.show()
+
+
 
 for ind in selected_indicators:
     df_year, df_country = data_ingestion(df, ind)
@@ -63,12 +72,13 @@ sns.heatmap(df1[selected_indicators].corr(), cmap='viridis', linewidths=.5, anno
 
 
 
-# ## Correlation Graph of Countries for "Arable Land" Indicator
+# Correlation for Graphs
 
 df_year, df_country = data_ingestion(df, 'Arable land (% of land area)')
 
 plt.figure(figsize=(10,7))
 sns.heatmap(df_country[countries].corr(), cmap='viridis', linewidths=.5, annot=True)
+
 
 # getting data ready. removing some Regions and join data to get only the countries data.
 
