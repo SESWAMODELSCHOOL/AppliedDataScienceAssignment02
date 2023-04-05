da64af27624016865472bcab5de1f6051c089b6c
Muhammad Ali <127026879+SESWAMODELSCHOOL@users.noreply.github.com>
2023-04-05 21:57:40 +0100

    Update AppliedDataScienceAssign02.py

--- AppliedDataScienceAssign02.py
+++ AppliedDataScienceAssign02.py
@@ -16,3 +16,8 @@ df = df[['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code','200
 
 df['mean'] = df[[ '2005', '2006', '2007','2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
        '2017', '2018', '2019', '2020', '2021']].mean(axis=1)
+selected_indicators = ['Urban population (% of total population)', 'Urban population growth (annual %)', 'Population, total', 'Population growth (annual %)',
+ 'Agricultural land (sq. km)', 'Energy use (kg of oil equivalent per capita)','Electric power consumption (kWh per capita)','Access to electricity (% of population)','Forest area (% of land area)','Arable land (% of land area)', 'Agricultural land (% of land area)']
+
+countries = ['Italy','Australia','China','United Arab Emirates','Germany','France',
+             'Malaysia','Japan','New Zealand','Morocco','United States','Pakistan','India','Bangladesh','Thailand']
