1ce5a8d48651871f2967190e4fea4fb4f380ad24
Muhammad Ali <127026879+SESWAMODELSCHOOL@users.noreply.github.com>
2023-04-05 21:29:15 +0100

    Update AppliedDataScienceAssign02.py

--- AppliedDataScienceAssign02.py
+++ AppliedDataScienceAssign02.py
@@ -11,4 +11,8 @@ df = pd.read_csv('API_19_DS2_en_csv_v2_4756035.csv',skiprows=4)
 dfc = pd.read_csv('Metadata_Country_API_19_DS2_en_csv_v2_4756035.csv')
 
 dfc1 = dfc[~pd.isna(dfc['IncomeGroup'])]
+df = df[['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code','2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012',
+         '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021']]
 
+df['mean'] = df[[ '2005', '2006', '2007','2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016',
+       '2017', '2018', '2019', '2020', '2021']].mean(axis=1)
